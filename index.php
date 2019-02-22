1e6d13f729969665fccfe95f689d51cb4613efc6
muhammadniko <mn.muhammadniko@gmail.com>
2019-02-22 22:41:56 +0800

    Application Index File

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php require_once "functions.php"; require_once "themes/header.php"; ?>
+	<!-- Page content of course! -->
+	<main class="bs-docs-masthead" id="content" role="main">
+  		<div class="container">
+   			<img src="<?php site('url');?>assets/images/RB.png" style="width:150px; height:150px;">
+    		<p class="lead"><h1>Pasang <b>IKLAN</b> di <br>Harian Pagi <b>Radar Banjarmasin</b></h1></p>
+    		<p class="lead"><br>
+				<a href="<?php site('url');?>iklan/order.php" class="btn btn-primary">Order Sekarang</a>
+    		</p>    
+  		</div>
+	</main>
+<?php require_once "themes/footer.php"; ?>
