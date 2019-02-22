82576cbcecb627a099de716bd0530fc661154254
muhammadniko <mn.muhammadniko@gmail.com>
2019-02-22 23:01:48 +0800

    About Page File

---
+++ tentang.php
@@ -0,0 +1,13 @@
+<?php require_once "functions.php"; require_once "themes/header.php"; ?>
+	<!-- Page content of course! -->
+	<main class="bs-docs-masthead" id="content" role="main">
+  		<div class="container">
+
+   			<p class="lead"><b>TENTANG KAMI</b></p>
+   			<img src="<?php site('url');?>assets/images/me.jpg" style="border-radius: 100px;width:100px; height:100px;">
+   			<p style="color: white;font-size: 21px"><br><b>Tim Pengembang<br>
+   			Muhammad Niko D. Jatmiko(16031076)</b></p>
+
+  		</div>
+	</main>
+<?php require_once "themes/footer.php"; ?>
