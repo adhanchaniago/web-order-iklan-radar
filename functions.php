cbbe5ec2896c9708f29d42272a75aca41c252903
muhammadniko <mn.muhammadniko@gmail.com>
2019-02-22 22:41:06 +0800

    Functions Loader File

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+	include "system/config.php";
+	include "system/connection.php";
+	include "system/sites.php";
+	include "functions/Iklan.php";
+	include "functions/Users.php";
+	include "functions/Pelanggan.php";
+?>
