f4f0f63a786f4a6a17d9058e4fe6c1d9844e2ffc
muhammadniko <mn.muhammadniko@gmail.com>
2019-02-23 20:25:46 +0800

    Change developer name

--- tentang.php
+++ tentang.php
@@ -6,7 +6,7 @@
    			<p class="lead"><b>TENTANG KAMI</b></p>
    			<img src="<?php site('url');?>assets/images/me.jpg" style="border-radius: 100px;width:100px; height:100px;">
    			<p style="color: white;font-size: 21px"><br><b>Tim Pengembang<br>
-   			Muhammad Niko D. Jatmiko(16031076)</b></p>
+   			Muhammad Niko (16031076)</b></p>
 
   		</div>
 	</main>
